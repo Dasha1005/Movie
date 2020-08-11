d0dc980e421d20078f506216ab5c28b3b2d6c060
Dasha <rosinecdasa@gmail.com>
2020-08-11 19:45:40 +0300

    add arrows to slick-slider

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ $(function(){
         prevArrow: '<button type="button" class="slick-btn slick-prev">' +
             '<img src="images/slider-prev-arrow.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next">' +
-            '<img src="images/slider-next-arrow.svg" alt=""></button>',
-        autoplay:true,
-        fade:true
+            '<img src="images/slider-next-arrow.png" alt=""></button>',
     });
 });
