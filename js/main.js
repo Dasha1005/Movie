dbfbc047232f7d0dd64a3d4553aefe839ee45ab3
Dasha <rosinecdasa@gmail.com>
2020-08-11 20:53:09 +0300

    changed style of slider-arrows

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 $(function(){
     $('.slider').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev">' +
-            '<img src="images/slider-prev-arrow.svg" alt=""></button>',
+            '<img src="images/previous-arrow.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next">' +
-            '<img src="images/slider-next-arrow.png" alt=""></button>',
+            '<img src="images/next-arrow.svg" alt=""></button>',
+        infinite: true
     });
 });
